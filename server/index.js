6f4d24c2dfb5171e1d7bdd8b6b30bef5ad243d51
Andy Kim <andyhyukim@gmail.com>
2025-03-18 15:50:09 -0400

    Connect server with Postgres

--- server/index.js
+++ server/index.js
@@ -5,12 +5,27 @@ import pg from 'pg';
 
 dotenv.config();
 
+const { Pool } = pg;
+const pool = new Pool({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+});
+
 const app = express();
 app.use(cors());
 const PORT = 3000;
 
 app.get('/api/test', async (req, res) => {
-    res.json({ message: 'hello' });
+    try {
+        const result = await pool.query('SELECT * FROM test_users');
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
 app.listen(PORT, () => {
