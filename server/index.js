07d0b49e62b1a81d15589ee3ac30d0827d2c7c93
Andy Kim <andyhyukim@gmail.com>
2025-03-18 15:22:51 -0400

    Use additional libraries for server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import pg from 'pg';
+
+dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = 3000;
 
 app.get('/api/test', async (req, res) => {
