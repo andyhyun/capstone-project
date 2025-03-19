f7896b9f026e93818c8e156f4becc8cc57163f72
Andy Kim <andyhyukim@gmail.com>
2025-03-19 14:16:33 -0400

    Add basic Flask functionality

--- react/components/Employee.jsx
+++ react/components/Employee.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Employee = (props) => {
+    const [flaskData, setFlaskData] = useState({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://localhost:5000/api/test`);
+                if (!response.ok) {
+                    throw new Error("Failed to fetch user data");
+                }
+                const data = await response.json();
+                setFlaskData(data);
+            } catch (err) {
+                console.error("Error fetching Flask data");
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className="card" style={{ flex: '1', minWidth: '300px', maxWidth: '45%' }}>
             <div className="card-body">
@@ -12,7 +30,7 @@ const Employee = (props) => {
                 <div className="card-text">Location: {props?.data.location}</div>
                 <div className="card-text">Job Role: {props?.data.job}</div>
                 <div className="card-text">Salary: {props?.data.salary}</div>
-                
+                <div className="card-text">Flask Data: {flaskData.message}</div>
             </div>
             
         </div>
